fb0d5519455b76032fe30eb8744913e4c4fc43ae
nunomcpereira <konkering@gmail.com>
2022-09-15 15:09:36 +0000

    new changes

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,18 +10,19 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
-    basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("banana"), 55);
 
-    // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("pear"),7);
 
+    basket.insert(String::from("apple"),11);
+    println!("{}",basket.len());
     basket
 }
 
@@ -32,6 +33,7 @@ mod tests {
     #[test]
     fn at_least_three_types_of_fruits() {
         let basket = fruit_basket();
+        
         assert!(basket.len() >= 3);
     }
 
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,20 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.to_owned().trim().to_owned()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut tempStr = String::from(input);
+    tempStr.push_str(&String::from(" world!"));
+    tempStr
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut tempStr = &input.replace("cars", "balloons");
+    tempStr.to_owned()
 }
 
 #[cfg(test)]

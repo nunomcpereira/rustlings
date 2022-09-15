0538a245b7b6c5c9c6c69b295b82170aedfb4502
nunomcpereira <konkering@gmail.com>
2022-09-15 14:12:55 +0000

    solutions

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,28 +1,18 @@
-// move_semantics2.rs
-// Make me compile without changing line 13 or moving line 10!
-// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
+pub fn main() {
+    let mut vec0 = Vec::new();
 
-// I AM NOT DONE
-
-fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.to_vec()
 }
--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,13 +4,12 @@
 // Make the code compile and the tests pass!
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
     sender_country: String,
     recipient_country: String,
-    weight_in_grams: i32,
+    weight_in_grams: i32
 }
 
 impl Package {
@@ -26,12 +25,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        &self.recipient_country != &self.sender_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        cents_per_gram * &self.weight_in_grams
     }
 }
 
